6946dd294a657b6b254728662c48a785e8096f58
Gaurav237 <gktiwary237@gmail.com>
2024-05-20 20:15:15 +0530

    dotenv and nodemon installed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const chats = require('./data/data')
 const app = express()
+dotenv.config()
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -15,6 +18,7 @@ app.get('/api/chat/:id', (req, res) => {
   const chat = chats.find(c => c._id == id )
   res.send(chat)
 })
+
 
 app.listen(3000, () => {
     console.log('Server started on PORT 3000')
